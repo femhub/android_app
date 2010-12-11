c8e25a742ba2d4da97c42d871081306e2fb08c96
Ondrej Certik <ondrej@certik.cz>
2010-12-10 20:17:04 -0800

    Add slider

--- myapp/src/com/example/helloandroid/HelloAndroid.java
+++ myapp/src/com/example/helloandroid/HelloAndroid.java
@@ -5,16 +5,27 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.view.View;
 
 public class HelloAndroid extends Activity
+    implements SeekBar.OnSeekBarChangeListener
 {
-    /** Called when the activity is first created. */
+    SeekBar seekbar;
+    EditText num1;
+    EditText num2;
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.main);
+
+        this.seekbar = (SeekBar) this.findViewById(R.id.seekbar);
+        this.seekbar.setOnSeekBarChangeListener(this);
+        this.seekbar.setMax(100);
+        this.num1 = (EditText) this.findViewById(R.id.num1);
+        this.num2 = (EditText) this.findViewById(R.id.num2);
+
         final Button button = (Button) this.findViewById(R.id.run);
         final HelloAndroid self = this;
         button.setOnClickListener(new View.OnClickListener() {
@@ -24,13 +35,22 @@ public class HelloAndroid extends Activity
         });
     }
 
-    public void run()
-    {
-        final EditText num1 = (EditText) this.findViewById(R.id.num1);
-        final EditText num2 = (EditText) this.findViewById(R.id.num2);
+    public void run() {
         final CharSequence t = "num1 = " + num1.getText() + "; num2 = " +
             num2.getText() + ";";
         Toast.makeText(this.getApplicationContext(), t,
                 Toast.LENGTH_SHORT).show();
+    }
+
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
+        this.num2.setText(progress + " " + fromTouch);
+    }
+
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        this.num1.setText("start");
+    }
+
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        this.num1.setText("stop");
     }
 }
