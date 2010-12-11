f0ae7c1db3ec61c0355516b0c0867d43503104ac
Ondrej Certik <ondrej@certik.cz>
2010-12-10 19:57:39 -0800

    Add a callback

--- myapp/src/com/example/helloandroid/HelloAndroid.java
+++ myapp/src/com/example/helloandroid/HelloAndroid.java
@@ -2,6 +2,9 @@ package com.example.helloandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View;
 
 public class HelloAndroid extends Activity
 {
@@ -11,5 +14,12 @@ public class HelloAndroid extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        final Button button = (Button) findViewById(R.id.run);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                    "Run clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
